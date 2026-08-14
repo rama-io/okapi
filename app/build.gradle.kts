8ac824a00a64739f8ec2673cef64f3314327b347
Pombo <38018741+pomboverso@users.noreply.github.com>
2026-08-14 01:56:50 -0700

    3 (#5)
    * bump version
    
    * Update video.txt
    
    * making the year static
    
    * adding sha-256

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,10 @@
-import java.time.LocalDate
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
 }
 
-val currentYear = LocalDate.now().year
+val currentYear = 2026
 
 android {
     namespace = "com.rama.okapi"
@@ -15,7 +14,7 @@ android {
         applicationId = "com.rama.okapi"
         minSdk = 21
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "$currentYear.$versionCode"
     }
 
