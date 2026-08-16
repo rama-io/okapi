ac2c9b4d199ae0cd0ecfeb39897781174197ed4e
Pombo <38018741+pomboverso@users.noreply.github.com>
2026-08-16 11:41:37 -0700

    4 (#6)
    * bump version
    
    * add catalogue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.rama.okapi"
         minSdk = 21
         targetSdk = 37
-        versionCode = 3
+        versionCode = 4
         versionName = "$currentYear.$versionCode"
     }
 
